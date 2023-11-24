53ced887bd0c44052469a484ae2f6c029f61d0c1
youngseojeon <20wjsdudtj@gmail.com>
2023-11-24 11:14:39 +0900

    Exception을 위한 BaseResponse 클래스 추가

---
+++ demo/src/main/java/com/example/demo/config/BaseException.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class BaseException extends Exception {
+    private BaseResponseStatus status;
+}
---
+++ demo/src/main/java/com/example/demo/config/BaseResponseStatus.java
@@ -0,0 +1,34 @@
+package com.example.demo.config;
+
+import lombok.Getter;
+
+@Getter
+public enum BaseResponseStatus {
+
+    /**
+     * 1000: 요청 성공
+     */
+    SUCCESS(true, 1000, "요청에 성공하였습니다."),
+
+    /**
+     *  2000: Request 오류
+     */
+    // Students(2000~2099)
+    NULL_STUDENT(false, 2000, "일치하는 학생 값이 없습니다."),
+
+
+    /**
+     * 4000: DB, Server 오류
+     */
+    DATABASE_ERROR(false, 4000, "데이터베이스 연결에 실패했습니다.");
+
+    private final boolean isSuccess;
+    private final int code;
+    private final String message;
+
+    private BaseResponseStatus(boolean isSuccess, int code, String message) {
+        this.isSuccess = isSuccess;
+        this.code = code;
+        this.message = message;
+    }
+}
