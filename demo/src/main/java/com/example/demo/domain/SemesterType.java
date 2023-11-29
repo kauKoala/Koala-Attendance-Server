f9f7521bd52dadcfed049d8f122364c5b2f1ecb4
youngseojeon <20wjsdudtj@gmail.com>
2023-11-29 15:30:11 +0900

    학기 Type을 Enum 타입으로 저장

---
+++ demo/src/main/java/com/example/demo/domain/SemesterType.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain;
+
+public enum SemesterType {
+    FIRST_SEMESTER,
+    SECOND_SEMESTER,
+    SUMMER_VACATION,
+    WINTER_VACATION,
+    // 필요에 따라 추가적인 학기나 방학을 나타내는 값들을 포함할 수 있음
+}
+
--- demo/src/main/java/com/example/demo/domain/Week.java
+++ demo/src/main/java/com/example/demo/domain/Week.java
@@ -12,7 +12,7 @@ public class Week {
     @GeneratedValue
     private Long id;
     private int weekNumber; //1주차, 2주차 ..
-    private Long semesterId; //학기(1학기, 2학기, 여름방학...)와 연동
+    private SemesterType semesterType; //학기(1학기, 2학기, 여름방학...)와 연동
 
     private LocalDateTime startDate; // 시작 날짜
     private LocalDateTime endDate; // 종료 날짜
