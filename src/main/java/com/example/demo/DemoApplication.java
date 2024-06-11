9a294b90acd0f4029aa5e39272ccbb9996b96337
전영서 <20wjsdudtj@gmail.com>
2024-06-11 14:04:27 +0900

    test

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -15,7 +15,6 @@ public class DemoApplication extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder builder){
         return super.configure(builder);
